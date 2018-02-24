704b8682b8c501a3928b518082abbb02ab83ed10
Kristian Sakarisson <kristian@sakarisson.com>
2018-02-24 11:44:36 +0200

    Added HanoiEngine class

---
+++ DP Command/HanoiEngine.cpp
@@ -0,0 +1,25 @@
+#include "HanoiEngine.h"
+
+HanoiEngine::HanoiEngine() {
+
+}
+
+void HanoiEngine::show(std::ostream & aStrm) {
+
+}
+
+bool HanoiEngine::move(int aFrom, int aTo) {
+    return false;
+}
+
+void HanoiEngine::reset(int aDiscs) {
+
+}
+
+bool HanoiEngine::isDone() {
+    return false;
+}
+
+HanoiEngine::~HanoiEngine() {
+
+}
---
+++ DP Command/HanoiEngine.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <ostream>
+#include <iostream>
+#include <deque>
+
+class HanoiEngine {
+public:
+    HanoiEngine();
+    ~HanoiEngine() {}
+
+    // Display the towers with the discs
+    void show(std::ostream& aStrm = std::cout);
+
+    // Move a disc from tower aFrom to tower aTo.
+    // A successful move returns true
+    bool move(int aFrom, int aTo);
+
+    // Reset the game, start with aDiscs discs
+    void reset(int aDiscs);
+
+    // Is the game successfully finished?
+    bool isDone();
+
+private:
+    enum { T1, T2, T3 };
+    int iDiscs;
+    int iMoves;
+    std::deque<int> iTowers[3];
+};
+
