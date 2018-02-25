96d106e63921694dc55fbac3b4fc962012fead7e
Kristian Sakarisson <kristian@sakarisson.com>
2018-02-25 18:07:11 +0200

    Added menu options

--- DP Command/GameInterface.cpp
+++ DP Command/GameInterface.cpp
@@ -1,12 +1,14 @@
 #include "GameInterface.h"
 
 #include <iostream>
+#include <string>
 
 using std::endl;
 
 GameInterface::GameInterface() {
     _hanoiEngine = std::make_unique<HanoiEngine>();
     _commandManager = std::make_unique<CommandManager>();
+    lastMenuResult = "Welcome";
 }
 
 GameInterface::~GameInterface() {
@@ -25,6 +27,8 @@ void GameInterface::showGame() {
 
 void GameInterface::showMenu() {
     std::cout <<
+        lastMenuResult << endl <<
+        "====================================" << endl <<
         "1. Replay last game" << endl <<
         "2. Move" << endl <<
         "3. Undo" << endl <<
@@ -48,4 +52,44 @@ void GameInterface::tick() {
         "============================================================" << endl;
     this->showMenu();
     int choice = this->getUserInput();
+    switch (choice) {
+    case 1:
+        this->replay();
+        break;
+    case 2:
+        this->move();
+        break;
+    case 3:
+        this->undo();
+        break;
+    case 4:
+        this->redo();
+        break;
+    case 5:
+        this->reset();
+        break;
+    default:
+        std::cout << "Invalid choice" << endl;
+    }
+    system("cls");
+}
+
+void GameInterface::replay() {
+
+}
+
+void GameInterface::move() {
+
+}
+
+void GameInterface::undo() {
+
+}
+
+void GameInterface::redo() {
+
+}
+
+void GameInterface::reset() {
+
 }
--- DP Command/GameInterface.h
+++ DP Command/GameInterface.h
@@ -15,7 +15,15 @@ private:
     void showMenu();
     int getUserInput();
     void tick(); // A single game "tick" - i.e. show game, get user input, process input
+
+    // Different menu options
+    void replay();
+    void move();
+    void undo();
+    void redo();
+    void reset();
 private:
     std::unique_ptr<HanoiEngine> _hanoiEngine;
     std::unique_ptr<CommandManager> _commandManager;
+    std::string lastMenuResult;
 };
